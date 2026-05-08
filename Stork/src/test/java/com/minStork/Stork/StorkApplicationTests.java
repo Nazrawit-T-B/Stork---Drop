36b45b634b152bd48198387531c260bdc1b72660
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-05-08 16:39:09 +0300

    Intitializing project structure for Stork application

---
+++ Stork/src/test/java/com/minStork/Stork/StorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minStork.Stork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
