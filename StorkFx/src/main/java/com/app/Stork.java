3116b420568637ba434c1c836b7aa455b7326cb9
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-05-08 18:30:12 +0300

    JavaFx implementation of stork App

--- StorkFx/src/main/java/com/app/Stork.java
+++ StorkFx/src/main/java/com/app/Stork.java
@@ -1,4 +1,22 @@
 package com.app;
 
-public class Stork {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Stork extends Application{
+    public void start(Stage stage){
+        BorderPane root=new BorderPane();
+        Scene scene=new Scene(root);
+
+        stage.setTitle("Stork");
+        stage.setResizable(true);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+
 }
