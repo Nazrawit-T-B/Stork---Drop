912623839fefd9102ea9de424a6efffc6c42b7a7
Kidst Nigus Gebreslase <kidstescape1995@gmail.com>
2026-05-29 23:57:37 +0300

    Add method to find user by authentication token

--- Stork/src/main/java/com/minStork/Stork/data/UserRepository.java
+++ Stork/src/main/java/com/minStork/Stork/data/UserRepository.java
@@ -12,4 +12,7 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByEmail(String email);
 
+    Optional<UserEntity> findByAuthToken(String authToken);
+
+
 }
