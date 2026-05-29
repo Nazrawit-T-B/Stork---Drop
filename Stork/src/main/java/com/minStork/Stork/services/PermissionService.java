6d5f3c62f18ce8b01776186227d5ac20c6d0a947
krbl-lmm <kirubellemma08@gmail.com>
2026-05-29 22:40:57 +0300

    Added makeOwner method in permission service

--- Stork/src/main/java/com/minStork/Stork/services/PermissionService.java
+++ Stork/src/main/java/com/minStork/Stork/services/PermissionService.java
@@ -1,13 +1,13 @@
 package com.minStork.Stork.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import com.minStork.Stork.data.PermissionRepository;
-import com.minStork.Stork.data.UserEntity;
 import com.minStork.Stork.data.FileEntity;
 import com.minStork.Stork.data.PermissionEntity;
+import com.minStork.Stork.data.PermissionRepository;
 import com.minStork.Stork.data.PermissionType;
-import org.springframework.stereotype.Service;
+import com.minStork.Stork.data.UserEntity;
 
 @Service
 public class PermissionService {
@@ -29,5 +29,13 @@ public class PermissionService {
 
     public void revokePermission(PermissionEntity permission) {
         permissionRepository.delete(permissionRepository.findByUserAndFile(permission.getUser(), permission.getFile()));
+    }
+
+    public void makeOwner(UserEntity user, FileEntity file) {
+        PermissionEntity permission = new PermissionEntity();
+        permission.setPermissionType(PermissionType.OWNER);
+        permission.setUser(user);
+        permission.setFile(file);
+        permissionRepository.save(permission);
     }
 }
