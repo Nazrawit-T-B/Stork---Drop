dd6e39cfeea05b95db0652c2a8bee04c94bbb024
krbl-lmm <kirubellemma08@gmail.com>
2026-05-31 00:30:36 +0300

    Enhance PermissionController and PermissionService with new endpoints and methods for file permissions management

--- Stork/src/main/java/com/minStork/Stork/controller/PermissionController.java
+++ Stork/src/main/java/com/minStork/Stork/controller/PermissionController.java
@@ -1,7 +1,13 @@
 package com.minStork.Stork.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +16,7 @@ import com.minStork.Stork.data.PermissionEntity;
 import com.minStork.Stork.services.PermissionService;
 
 @RestController
-@RequestMapping("/permissions")
+@RequestMapping("/api/permissions")
 public class PermissionController {
 
     private final PermissionService permissionService;
@@ -19,13 +25,24 @@ public class PermissionController {
         this.permissionService = permissionService;
     }
 
+    @GetMapping("/file/{id}")
+    public List<PermissionEntity> getFilePermissions(@PathVariable Long id) {
+        return permissionService.getFilePermissions(id);
+    }
+
     @PostMapping("/grant")
     public ResponseEntity<?> grantPermission(@RequestBody PermissionEntity permission) {
         permissionService.grantPermission(permission);
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/revoke")
+    @PutMapping("/update")
+    public ResponseEntity<?> updatePermission(@RequestBody PermissionEntity permission) {
+
+        return ResponseEntity.ok().build();
+    } 
+
+    @DeleteMapping("/revoke")
     public ResponseEntity<?> revokePermission(@RequestBody PermissionEntity permission) {
         permissionService.revokePermission(permission);
         return ResponseEntity.ok().build();
--- Stork/src/main/java/com/minStork/Stork/data/PermissionRepository.java
+++ Stork/src/main/java/com/minStork/Stork/data/PermissionRepository.java
@@ -1,13 +1,17 @@
 package com.minStork.Stork.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface PermissionRepository extends JpaRepository<PermissionEntity, Long> {
     PermissionEntity findByUserAndFile(UserEntity user, FileEntity file);
 
     List<PermissionEntity> findByUser(UserEntity user);
+
+    List<PermissionEntity> findByFile(FileEntity file);
+
+    List<PermissionEntity> findByFileId(Long userId);
 }
