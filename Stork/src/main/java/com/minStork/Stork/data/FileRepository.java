6371fb69847e402d6c35bf8e859413c9bd31928b
Kidst Nigus Gebreslase <kidstescape1995@gmail.com>
2026-06-01 04:26:58 +0300

    Add method to find public files in FileRepository

--- Stork/src/main/java/com/minStork/Stork/data/FileRepository.java
+++ Stork/src/main/java/com/minStork/Stork/data/FileRepository.java
@@ -10,4 +10,6 @@ import java.util.Optional;
 public interface FileRepository extends JpaRepository<FileEntity, Long> {
     Optional<FileEntity> findByFilename(String filename);
     Optional<FileEntity> findByFilenameAndOwner(String filename, UserEntity owner);
+    List<FileEntity> findByIsPublicTrue();
+
 }
